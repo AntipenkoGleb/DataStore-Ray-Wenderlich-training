cfbc8a753824f39a0a545359c2908f87566face3
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-09-11 23:51:40 +0300

    add google Gson library; add reading and writing drop data in shared prefs

--- app/src/main/java/com/raywenderlich/android/datadrop/model/SharedPrefsRepository.kt
+++ app/src/main/java/com/raywenderlich/android/datadrop/model/SharedPrefsRepository.kt
@@ -1,20 +1,26 @@
 package com.raywenderlich.android.datadrop.model
 
 import android.content.Context
+import com.google.gson.Gson
 import com.raywenderlich.android.datadrop.app.DataDropApplication
 
 object SharedPrefsRepository : DropRepository {
 
     private const val SHARED_PREFS_REPOSITORY = "SHARED_PREFS_REPOSITORY"
 
-    fun sharedPrefs() = DataDropApplication.getAppContext().getSharedPreferences(SHARED_PREFS_REPOSITORY, Context.MODE_PRIVATE)
+    private val gson = Gson()
+
+    private fun sharedPrefs() = DataDropApplication.getAppContext().getSharedPreferences(SHARED_PREFS_REPOSITORY, Context.MODE_PRIVATE)
 
     override fun addDrop(drop: Drop) {
-
+        sharedPrefs().edit().putString(drop.id, gson.toJson(drop)).apply()
     }
 
     override fun getDrops(): List<Drop> {
-        return emptyList()
+        return sharedPrefs().all.keys
+                .map { sharedPrefs().getString(it,"") }
+                .filterNot { it.isNullOrBlank() }
+                .map { gson.fromJson(it,Drop::class.java) }
     }
 
     override fun clearDrop(drop: Drop) {
