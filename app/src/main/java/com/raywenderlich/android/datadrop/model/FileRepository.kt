0545965a37c8b3f36b60a4f236ca1d8d62736b3a
Antipenko Gleb <antipenko.g.d@gmail.com>
2020-09-14 16:17:19 +0300

    add reading drop data from file

--- app/src/main/java/com/raywenderlich/android/datadrop/model/FileRepository.kt
+++ app/src/main/java/com/raywenderlich/android/datadrop/model/FileRepository.kt
@@ -5,9 +5,8 @@ import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.raywenderlich.android.datadrop.app.DataDropApplication
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
+import java.io.*
+import java.lang.StringBuilder
 
 object FileRepository : DropRepository {
 
@@ -33,7 +32,19 @@ object FileRepository : DropRepository {
     }
 
     override fun getDrops(): List<Drop> {
-        return emptyList()
+        val drops = mutableListOf<Drop>()
+        
+        try {
+            val fileList = getDirectory().list()
+
+            fileList.map { convertStreamToString(dropInputStream(it)) }.mapTo(drops) {
+                gson.fromJson(it, Drop::class.java)
+            }
+        } catch (e: IOException) {
+            Log.e("FIleRepository", "Error reading drop")
+        }
+
+        return drops
     }
 
     override fun clearDrop(drop: Drop) {
@@ -52,5 +63,22 @@ object FileRepository : DropRepository {
 
     private fun dropOutputStream(drop: Drop): FileOutputStream {
         return FileOutputStream(dropFile(dropFilename(drop)))
+    }
+
+    private fun dropInputStream(filename: String): FileInputStream {
+        return FileInputStream(dropFile(filename))
+    }
+
+    @Throws(IOException::class)
+    fun convertStreamToString(inputStream: FileInputStream): String {
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val sb = StringBuilder()
+        var line: String? = reader.readLine()
+        while (line != null) {
+            sb.append(line).append("\n")
+            line = reader.readLine()
+        }
+        reader.close()
+        return sb.toString()
     }
 }
